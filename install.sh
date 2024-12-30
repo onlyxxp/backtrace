81d1849c2b8a274fd446a915913f283628eb071f
onlyxxp <onlyxxp@gmail.com>
2024-12-30 22:31:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 arch=$(uname -m)
 if [ "$arch" = "x86_64" ]; then
-  wget -q -O backtrace.tar.gz  https://github.com/zhanghanyun/backtrace/releases/latest/download/backtrace-linux-amd64.tar.gz
+  wget -q -O backtrace.tar.gz  https://github.com/onlyxxp/backtrace/releases/latest/download/backtrace-linux-amd64.tar.gz
 else
-  wget -q -O backtrace.tar.gz  https://github.com/zhanghanyun/backtrace/releases/latest/download/backtrace-linux-arm64.tar.gz
+  wget -q -O backtrace.tar.gz  https://github.com/onlyxxp/backtrace/releases/latest/download/backtrace-linux-arm64.tar.gz
 fi
 
 tar -xf backtrace.tar.gz
