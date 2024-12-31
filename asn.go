c7ce14e071c0bf8ee1903389ca2d4c5836a304ee
xiaopeng <xiaoxinpeng@bytedance.com>
2024-12-31 10:58:52 +0800

    fmt

--- asn.go
+++ asn.go
@@ -20,7 +20,7 @@ var (
 		"成都电信", "成都联通", "成都移动"}
 	m = map[string]string{"AS4134": "电信163  [普通线路]", "AS4809": "电信CN2  [优质线路]", "AS4837": "联通4837 [普通线路]",
 		"AS9929": "联通9929 [优质线路]", "AS58807": "移动CMIN2[优质线路]", "AS9808": "移动CMI  [普通线路]",
-		"AS58453": "移动CMI  [普通线路]", "AS-CTG-CN": "电信CTG  [优化线路]", "AS4812": "电信跳墙  [优化线路]"}
+		"AS58453": "移动CMI  [普通线路]", "AS-CTG-CN": "电信CTG  [优化线路]", "AS4812": "电信bug  [跳墙线路]"}
 )
 
 func trace(ch chan Result, i int) {
@@ -52,7 +52,7 @@ func trace(ch chan Result, i int) {
 			case "AS-CTG-CN":
 				c = color.New(color.FgHiCyan).Add(color.Bold).SprintFunc()
 			case "AS4812":
-				c = color.New(color.FgHiMagenta).Add(color.Bold).SprintFunc()
+				c = color.New(color.FgHiCyan).Add(color.Bold).SprintFunc()
 			default:
 				c = color.New(color.FgWhite).Add(color.Bold).SprintFunc()
 			}
